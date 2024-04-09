fcda1f448d651a79dc82354fc4a63f7ccd938881
Naimad123x <wrx81745@student.wsb.wroclaw.pl>
2024-04-09 20:07:16 +0200

    blog update

--- src/web/routers/api/v1/blogApi.js
+++ src/web/routers/api/v1/blogApi.js
@@ -5,14 +5,15 @@ const randomNum = customAlphabet('1234567890', 5);
 
 const blogNew = async function(req, res) {
 
-  console.log(req.session.user)
+  if(!req.body.title || req.body.editor)
+    return res.sendStatus(400)
 
   await savePost(
     {
       author: req.session.user.user,
       text: req.body.editor,
       title: req.body.title,
-      image: req.body.image,
+      image: req.body.image ? "https://placehold.co/600x400?text=Image",
       link: encodeURIComponent(req.body.title.replace(/\s/g, "-")+"-"+randomNum()),
       date: Date.now()
     }
