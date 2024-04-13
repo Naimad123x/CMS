6692dcae434d254ebf9f48f0f0ad239a32cedeb2
Naimad123x <wrx81745@student.wsb.wroclaw.pl>
2024-04-13 14:14:08 +0200

    limiter update

--- src/utils/limiter.js
+++ src/utils/limiter.js
@@ -4,7 +4,7 @@ const ms = require("ms")
 const limiter = rateLimit({
   skip: (req) => req.url === '/reset',
   windowMs: ms("1m"), // 1 minute
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minute).
+  limit: 70, // Limit each IP to 100 requests per `window` (here, per 1 minute).
   standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 })
